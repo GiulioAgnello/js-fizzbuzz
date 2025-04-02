34e3c7353dcf53e012ad70c3fd42527d93d8b389
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-04-02 17:01:24 +0200

    end

--- jv/script.js
+++ jv/script.js
@@ -6,10 +6,11 @@ const fizzbuzz = "FizzBuzz";
 // esecuzione stabiliamo il ciclo e creiamo i multipli per cui far comparire
 
 for (let i = 1; i <= 100; i++) {
-  if (i % 3 === 0) {
-    console.log(i);
-  } else i % 5 === 0;
-  {
-    console.log(i);
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log(fizzbuzz);
+  } else if (i % 3 === 0) {
+    console.log(fizz);
+  } else if (i % 5 === 0) {
+    console.log(buzz);
   }
 }
