b617009df75cf28d72901ae0413449e4cec1d72b
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-04-02 15:46:57 +0200

    fiz&Buzz

--- jv/script.js
+++ jv/script.js
@@ -1,3 +1,14 @@
+// informazioni
 const fizz = "fizz";
 const buzz = "Buzz";
 const fizzBuzz = "FizzBuzz";
+
+// esecuzione
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 2 === 1) {
+    console.log("fizz");
+  } else if (i % 2 === 0) {
+    console.log("Buzz");
+  }
+}
